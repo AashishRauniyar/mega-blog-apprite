c3413eb63fdf1d387e621d4f8bdaa7cd25c86f42
AashishRauniyar <rauniyaaraashish@gmail.com>
2024-07-29 00:46:58 +0545

    appwrite-auth-service-futureproof

---
+++ src/appwrite/auth.js
@@ -0,0 +1,71 @@
+import conf from "../conf/conf";
+import { Client, ID } from "appwrite";
+
+
+// appwrite auth service
+
+export class AuthService {
+
+
+    client = new Client();
+    account;
+
+    constructor() {
+        // this takes reference to current client inside the classa
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(client);
+    }
+
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount){
+
+                // call another method
+                return this.login({email, password});
+            }else{
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // read from docs to see how we can login
+    async login({email, password}){
+        try {
+            return await this.account.createEmailSession(email, password);
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions('current');
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+
+}
+
+// making object of AuthService class
+const authService = new AuthService();
+
+// exporting object authService of class AuthService
+export default authService;
