db0b3e0c6bd79bdc15ebd5078294ed8692d9d874
AashishRauniyar <rauniyaaraashish@gmail.com>
2024-08-01 23:44:12 +0545

    pages-done

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-
-// container is just a box that wraps the children
-const Container = ({children}) => {
-  return <div className='w-full max-w-7xl mx-auto px-4 '>{children}</div>;
+function Container({children}) {
+  return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
   
 }
 
